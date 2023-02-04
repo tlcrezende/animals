8cc116be6107700a50d8a27476cfaa8d8f05e06a
Thiago <tlcrezende@gmail.com>
2023-02-04 15:35:07 -0300

    Add cards styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
-import ProfileCard from "./ProfileCard";
+import 'bulma/css/bulma.css'
+import ProfileCard from "./AnimalShow";
+import AlexaImage from './images/alexa.png'
+import CortanaImage from './images/cortana.png'
+import SiriImage from './images/siri.png'
 
 function App() {
 	return (
 			<div>
-					<div>Personal Digital Assistants</div>
+				<section className="hero is-primary">
+					<div className="hero-body">
+						<p className="title">Personal Digital Assistants</p>
+					</div>
+				</section>
 
-
-					<ProfileCard title="Alexa" handle="@alexa99"/>
-					<ProfileCard title="Cortana" handle="@cortana32"/>
-					<ProfileCard title="Siri" handle="@siri01"/>
+					<div className="container">
+						<section className="section">
+							<div className="columns">
+								<div className="column is-4">
+									<ProfileCard 
+									title="Alexa" 
+									handle="@alexa99" 
+									image={AlexaImage}
+									description="Alexa was created by Amazon and helps you buy thinks unsusale"
+									/>
+								</div>
+								<div className="column is-4">
+									<ProfileCard 
+									title="Cortana" 
+									handle="@cortana32" 
+									image={CortanaImage}
+									description="Alexa was created by Amazon and helps you buy thinks"
+									/>
+								</div>
+								<div className="column is-4">
+									<ProfileCard 
+									title="Siri" 
+									handle="@siri01" 
+									image={SiriImage}
+									description="Alexa was created by Amazon and helps you buy thinks"
+									/>
+								</div>
+							</div>
+						</section>
+					</div>
 
 			</div>
 	)
