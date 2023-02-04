0d636fdf2f8fb749c1a29b8b7a6b80120b188cb4
Thiago <tlcrezende@gmail.com>
2023-02-04 02:35:55 -0300

    rm comments, add props in ProfileCards

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
-// Import the React amd ReactDOM libreries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
-// Get a reference to the div with ID root
 const el = document.getElementById('root');
-
-// Tell React to take control of that element
 const root = ReactDOM.createRoot(el);
 
-// Create a component
-function App() {
-    return <h1>Hi there!</h1>;
-}
-// Show the component on the screen
 root.render(<App/>);
