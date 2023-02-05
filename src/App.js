c33185fb1f99edad163d927a4c0e85874e437636
Thiago <tlcrezende@gmail.com>
2023-02-04 23:28:57 -0300

    final version

--- src/App.js
+++ src/App.js
@@ -1,49 +1,29 @@
 import 'bulma/css/bulma.css'
-import ProfileCard from "./AnimalShow";
-import AlexaImage from './images/alexa.png'
-import CortanaImage from './images/cortana.png'
-import SiriImage from './images/siri.png'
+import { useState } from 'react'
+import AnimalShow from './AnimalShow';
+import './App.css'
+
+function getRandomAnimal() {
+	const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+	
+	return animals[Math.floor(Math.random() * animals.length)];
+}
 
 function App() {
+	const [animals, setAnimals] = useState([]);
+
+	const handleClick = () => {
+		setAnimals([...animals, getRandomAnimal()])
+	};
+
+	const renderedAnimals = animals.map((animal, index) => {
+		return <AnimalShow type={animal} key={index} />;
+	});
+
 	return (
-			<div>
-				<section className="hero is-primary">
-					<div className="hero-body">
-						<p className="title">Personal Digital Assistants</p>
-					</div>
-				</section>
-
-					<div className="container">
-						<section className="section">
-							<div className="columns">
-								<div className="column is-4">
-									<ProfileCard 
-									title="Alexa" 
-									handle="@alexa99" 
-									image={AlexaImage}
-									description="Alexa was created by Amazon and helps you buy thinks unsusale"
-									/>
-								</div>
-								<div className="column is-4">
-									<ProfileCard 
-									title="Cortana" 
-									handle="@cortana32" 
-									image={CortanaImage}
-									description="Alexa was created by Amazon and helps you buy thinks"
-									/>
-								</div>
-								<div className="column is-4">
-									<ProfileCard 
-									title="Siri" 
-									handle="@siri01" 
-									image={SiriImage}
-									description="Alexa was created by Amazon and helps you buy thinks"
-									/>
-								</div>
-							</div>
-						</section>
-					</div>
-
+			<div className="app">
+				<button onClick={handleClick}>Add Animal</button>
+				<div className="animal-list">{renderedAnimals}</div>
 			</div>
 	)
 }
